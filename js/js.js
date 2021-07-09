41c480d54b020e66bf21e628e7188e1d0d13ac9c
Kasra Ghavidel <32060084+kasraghavidel@users.noreply.github.com>
2021-07-09 20:31:34 +0430

    a

--- js/js.js
+++ js/js.js
@@ -2,7 +2,10 @@ let cat_section = document.querySelectorAll(".cat");
 window.addEventListener('scroll', function(){
     setTimeout(function(){
     let nowScroll = window.pageYOffset;
-
+    swiper.on('slideChange', function () {
+        swiper.params.hashNavigation.enabled = true;
+      });
+    
     for (let i = 0; i < cat_section.length; i++) {
         let secStart = cat_section[i].offsetTop;
         let secEnd = cat_section[i].scrollHeight/2 + cat_section[i].offsetTop;
@@ -10,13 +13,15 @@ window.addEventListener('scroll', function(){
             let goTo = cat_section[i].dataset.link;
             let slideLink = document.querySelectorAll('.swiper-slide a');
             slideLink.forEach(thisSlideLink => {
-                if (thisSlideLink.attributes.href.value == goTo ) {
+                if (thisSlideLink.attributes.href.value == goTo) {
                         swiper.slideTo(i,500, false);
+                        swiper.params.hashNavigation.enabled = false;
                 }
             })
         } else {
             continue;
         }
+        //swiper.params.hashNavigation.enabled = true;
     }
-
 }) ,10});
+
